852151916fc93cb6d73bdfdb07bc475e1581a1f6
Renier Strydom <renierbs@gmail.com>
2016-12-12 22:34:14 +0200

    Add leave_application table and fields

---
+++ database/migrations/2016_12_12_200455_create_leave_applications.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLeaveApplications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leave_applications', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->integer('status');
+            $table->decimal('days', 3, 2);
+            $table->timestamp('starts_date')->nullable();
+            $table->timestamp('return_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leave_applications');
+    }
+}
