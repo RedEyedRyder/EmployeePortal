a0c7d72a5ba8d7def4af30ecbc22402cd08af572
Renier Strydom <renierbs@gmail.com>
2016-12-13 23:45:45 +0200

    Update leave allowances to leave types table

--- database/migrations/2016_12_13_202938_create_leave_allowances_table.php
+++ database/migrations/2016_12_13_202938_create_leave_allowances_table.php
@@ -27,6 +27,6 @@ class CreateLeaveAllowancesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('leave_allowances');
+        Schema::dropIfExists('leave_types');
     }
 }
