5c73eae914f2661ecd7ae010e6d60adbcd1c16e5
Ryan <fishr01@gmail.com>
2016-12-13 22:41:13 +0200

    Created leave application seeder

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -11,6 +11,9 @@
 |
 */
 
+use Carbon\Carbon;
+
+
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
@@ -20,5 +23,19 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'email' => $faker->unique()->safeEmail,
         'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\LeaveApplication::class, function (Faker\Generator $faker) {
+	$date = Carbon::create(2016, 5, 28, 0, 0, 0);
+
+    return [
+        'user_id' => function () {
+            return factory(App\User::class)->create()->id;
+        },
+        'status' => $faker->numberBetween(0, 2),
+        'days' => $faker->numberBetween(1, 10),
+        'starts_date' => $date->format('Y-m-d H:i:s'),
+        'return_date' => $date->addDays(rand(1, 10))->format('Y-m-d H:i:s')
     ];
 });
